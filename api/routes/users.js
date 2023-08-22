e557bdabf0266a3ac627300462100a7c75cef097
Jeet Agrawal (jea316) <jea316@mail.usask.ca>
2023-08-22 00:48:24 -0600

    Added verify admin functionality

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { deleteUser, getAllUsers, getUser, updateUser } from "../controllers/userController.js";
-import { verifyToken, verifyUser } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
@@ -10,6 +10,10 @@ router.get("/checkauthentication", verifyToken, (req,res,next) => {
 
 router.get("/checkuser/:id", verifyUser, (req,res,next) => {
     res.send("Hey there user, you are logged in! and you can delete your account!")
+});
+
+router.get("/checkadmin/:id", verifyAdmin, (req,res,next) => {
+    res.send("Hey there admin, you are logged in! and you can delete all accounts!")
 });
 
 //GET
