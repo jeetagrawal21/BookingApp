f36439754c14051c7610b78c0df4bce0af912900
Jeet Agrawal (jea316) <jea316@mail.usask.ca>
2023-08-23 00:22:24 -0600

    Added verify user and verifyAdmin to update, create, and delete users or hotels.

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,18 +2,19 @@ import express from "express"
 import { createError } from "../utils/error.js";
 import { createHotel, deleteHotel, getAllHotel, getHotel, updateHotel } from "../controllers/hotelController.js";
 import jwt from "jsonwebtoken";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/", createHotel);
+router.post("/",verifyAdmin, createHotel);
 //GET
 router.get("/:id", getHotel);
 //GETALL
 router.get("/", getAllHotel);
 //UPDATE
-router.put("/:id", updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 //DELETE
-router.delete("/:id", deleteHotel);
+router.delete("/:id",verifyAdmin, deleteHotel);
 
 export default router
--- api/routes/users.js
+++ api/routes/users.js
@@ -4,25 +4,25 @@ import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req,res,next) => {
-    res.send("Hey there user, you are logged in!")
-});
+// router.get("/checkauthentication", verifyToken, (req,res,next) => {
+//     res.send("Hey there user, you are logged in!")
+// });
 
-router.get("/checkuser/:id", verifyUser, (req,res,next) => {
-    res.send("Hey there user, you are logged in! and you can delete your account!")
-});
+// router.get("/checkuser/:id", verifyUser, (req,res,next) => {
+//     res.send("Hey there user, you are logged in! and you can delete your account!")
+// });
 
-router.get("/checkadmin/:id", verifyAdmin, (req,res,next) => {
-    res.send("Hey there admin, you are logged in! and you can delete all accounts!")
-});
+// router.get("/checkadmin/:id", verifyAdmin, (req,res,next) => {
+//     res.send("Hey there admin, you are logged in! and you can delete all accounts!")
+// });
 
 //GET
-router.get("/:id", getUser);
+router.get("/:id",verifyUser, getUser);
 //GETALL
-router.get("/", getAllUsers);
+router.get("/",verifyAdmin, getAllUsers);
 //UPDATE
-router.put("/:id", updateUser);
+router.put("/:id",verifyUser, updateUser);
 //DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id",verifyUser, deleteUser);
 
 export default router
