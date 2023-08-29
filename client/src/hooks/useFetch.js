974457c247c066e58d31ecc0bdf9a2a97283cf00
Jeet Agrawal (jea316) <jea316@mail.usask.ca>
2023-08-28 23:50:11 -0600

    Fetching data from the backend to frontend.

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,12 +1,44 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
-    const useFetch = (url) => {
 
-        const [data, setData] = useState([]);
-        const [loading, setLoading] = useState(false);
-        const [error, setError] = useState(false);
-    }
-
+    useEffect(() => {
     
+        const fetchData = async () => {
+    
+            try {
+                setLoading(true);
+                const res = await axios.get(url);
+                setData(res.data);
+                // setError(false);
+            } catch (err) {
+                setError(true);
+            }
+            setLoading(false);
 
-}
+        };
+        fetchData();
+    }, [url])
+
+const reFetch = async () => {
+    
+    try {
+        setLoading(true);
+        const res = await axios.get(url);
+        setData(res.data);
+        // setError(false);
+    } catch (err) {
+        setError(true);
+    }
+    setLoading(false);
+};
+    
+    return { data, loading, error, reFetch };
+};
+
+export default useFetch;
