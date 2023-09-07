ed7991e202f8e34fc27bb8b1bd9109350c9c6826
Jeet Agrawal (jea316) <jea316@mail.usask.ca>
2023-09-06 23:51:44 -0600

    Added getByType method

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,12 +1,12 @@
 import express from "express"
 import { createError } from "../utils/error.js";
-import { countByCity, createHotel, deleteHotel, getAllHotel, getHotel, updateHotel } from "../controllers/hotelController.js";
+import { countByCity, countByType, createHotel, deleteHotel, getAllHotel, getHotel, updateHotel } from "../controllers/hotelController.js";
 import jwt from "jsonwebtoken";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-//CREATE
+//CREATE0
 router.post("/",verifyAdmin, createHotel);
 //GET
 router.get("/find/:id", getHotel);
@@ -18,7 +18,7 @@ router.put("/:id", verifyAdmin, updateHotel);
 router.delete("/:id",verifyAdmin, deleteHotel);
 
 router.get("/countByCity", countByCity);
-router.get("/countByType", getHotel);
+router.get("/countByType", countByType);
 
 
 export default router
